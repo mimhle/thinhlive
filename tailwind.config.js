6dd490366f9bae8306aa14b7d6979163d5b32891
mimhle <40392166+mimhle@users.noreply.github.com>
2024-09-17 10:30:11 +0700

    Refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,7 @@ module.exports = {
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
-            },
-        },
+        extend: {},
     },
     plugins: [
         daisyui

fe6a7281f890afa9402ecc5f684eba88f72b5f47
mimhle <40392166+mimhle@users.noreply.github.com>
2024-09-28 15:25:55 +0700

    Refactor

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,4 +1,16 @@
-export default function Nav({ children }) {
+"use server";
+
+import { cookies } from "next/headers";
+import { verifySession } from "/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Nav({ children }) {
+    const cookieStore = cookies();
+
+    if (await verifySession(cookieStore.get("username")?.value, cookieStore.get("session_id")?.value)) {
+        redirect("/");
+    }
+
     return <div>
         {children}
     </div>;

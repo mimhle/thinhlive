07cee1d7042e99ed9183047ea3ed6575c1dec842
NGUYỄN PHÚC THỊNH <119058000+hieu79115@users.noreply.github.com>
2024-11-05 01:34:32 +0700

    add CoverPhoto component

--- src/app/(main)/profile/page.js
+++ src/app/(main)/profile/page.js
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import Gallery from "@/app/(main)/Gallery";
 import Stat from "./Stat";
 import Avatar from "../Avatar";
+import CoverPhoto from "@/app/(main)/CoverPhoto";
 
 export default function Profile() {
     const cookieStore = cookies();
@@ -12,11 +13,7 @@ export default function Profile() {
     return <div className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex justify-center">
         <div className="bg-neutral w-4/5 h-fit flex flex-col drop-shadow-2xl">
             <div className="w-full h-fit">
-                <div className="w-full h-fit">
-                    <div className="w-full">
-                        <img src='./banner1.png' />
-                    </div>
-                </div>
+                <CoverPhoto/>
                 <div className="w-full px-4 -translate-y-1/3">
                     <div className="flex flex-row items-center gap-4">
                         <div className="w-40">
--- src/app/(main)/setting/page.js
+++ src/app/(main)/setting/page.js
@@ -3,9 +3,13 @@
 import {useEffect, useState} from "react";
 import {uploadMedia, setUserData, getUserData} from "/lib/frontend/api";
 import Avatar from "@/app/(main)/Avatar";
+import CoverPhoto from "@/app/(main)/CoverPhoto";
 
 export default function Setting() {
     const [data, setData] = useState(null);
+    const [avatarFile, setAvatarFile] = useState(null);
+    const [coverPhotoFile, setCoverPhotoFile] = useState(null);
+
 
     useEffect(() => {
         getUserData().then(r => {
@@ -20,11 +24,45 @@ export default function Setting() {
             className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex justify-center p-20">
         </div>
     }
+
+    const avatarUpdate = () => {
+        if (avatarFile) {
+            uploadMedia(avatarFile).then(r => {
+                if (r.status === "success") {
+                    setUserData(data.username, {avatar: r.result.url}).then(result => {
+                        console.log(result);
+                        window.location.reload();
+                    });
+                }
+            });
+        } else {
+            alert("Please select a file for the profile picture.");
+        }
+    }
+
+    const coverPhotoUpdate = () => {
+        if (coverPhotoFile) {
+            uploadMedia(coverPhotoFile).then(r => {
+                if (r.status === "success") {
+                    setUserData(data.username, {coverphoto: r.result.url}).then(result => {
+                        console.log(result);
+                        window.location.reload();
+                    });
+                }
+            });
+        } else {
+            alert("Please select a file for the profile picture.");
+        }
+    }
+
     return <div
-        className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex flex-col gap-4 p-20">
+        className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex flex-col gap-4 py-20 px-40">
         <div className="w-full h-fit bg-white/5 rounded-2xl">
             <div className="p-10 flex flex-col gap-8">
-                <span className="font-extrabold text-2xl">Profile Picture</span>
+                <div className="flex justify-between">
+                    <span className="font-extrabold text-2xl">Profile Picture</span>
+                    <button className="btn btn-success" onClick={avatarUpdate}>Update profile Picture</button>
+                </div>
                 <div className="flex flex-row gap-8 items-center w-full h-fit">
                     <div className="w-48">
                         <Avatar/>
@@ -34,16 +72,7 @@ export default function Setting() {
                             type="file"
                             className="file-input file-input-ghost w-full max-w-xs"
                             accept="image/*"
-                            onChange={e => {
-                                uploadMedia(e.target.files[0]).then(r => {
-                                    if (r.status === "success") {
-                                        setUserData(data.username, {avatar: r.result.url}).then(result => {
-                                            console.log(result);
-                                            window.location.reload();
-                                        });
-                                    }
-                                });
-                            }}
+                            onChange={e => setAvatarFile(e.target.files[0])}
                         />
                         <span className="text-white/50 mx-5">Must be JPG, PNG, JPEG and smaller 25MB</span>
                     </div>
@@ -52,26 +81,18 @@ export default function Setting() {
         </div>
         <div className="w-full h-fit bg-white/5 rounded-2xl">
             <div className="p-10 flex flex-col gap-8">
-                <span className="font-extrabold text-2xl">Cover Picture</span>
+                <div className="flex justify-between">
+                    <span className="font-extrabold text-2xl">Cover Picture</span>
+                    <button className="btn btn-success" onClick={coverPhotoUpdate}>Update Cover Picture</button>
+                </div>
                 <div className="flex flex-col gap-8 w-full h-fit">
-                    <div className="bg-black/50 flex items-center justify-center w-4/5 h-[400px] rounded-2xl">
-                        image
-                    </div>
+                    <CoverPhoto/>
                     <div className="flex flex-col gap-2">
                         <input
                             type="file"
                             className="file-input file-input-ghost w-full max-w-xs"
                             accept="image/*"
-                            onChange={e => {
-                                uploadMedia(e.target.files[0]).then(r => {
-                                    if (r.status === "success") {
-                                        setUserData(data.username, {avatar: r.result.url}).then(result => {
-                                            console.log(result);
-                                            window.location.reload();
-                                        });
-                                    }
-                                });
-                            }}
+                            onChange={e => {setCoverPhotoFile(e.target.files[0])}}
                         />
                         <span className="text-white/50 mx-5">Must be JPG, PNG, JPEG and smaller 25MB</span>
                     </div>

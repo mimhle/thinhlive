5046518c5d65046287ac1149a3604d0573dceb98
NGUYỄN PHÚC THỊNH <119058000+hieu79115@users.noreply.github.com>
2024-11-04 23:28:38 +0700

    change img to Image nextjs

--- src/app/(main)/setting/page.js
+++ src/app/(main)/setting/page.js
@@ -1,8 +1,7 @@
-
 "use client";
 
-import { useEffect, useState } from "react";
-import { uploadMedia, setUserData, getUserData } from "/lib/frontend/api";
+import {useEffect, useState} from "react";
+import {uploadMedia, setUserData, getUserData} from "/lib/frontend/api";
 import Avatar from "@/app/(main)/Avatar";
 
 export default function Setting() {
@@ -17,15 +16,46 @@ export default function Setting() {
     }, []);
 
     if (!data) {
-        return <></>;
+        return <div
+            className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex justify-center p-20">
+        </div>
     }
-    return <div className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex justify-center p-20">
+    return <div
+        className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex flex-col gap-4 p-20">
         <div className="w-full h-fit bg-white/5 rounded-2xl">
             <div className="p-10 flex flex-col gap-8">
                 <span className="font-extrabold text-2xl">Profile Picture</span>
-                <div className="flex gap-8 items-center w-full h-fit">
-                    <div className="h-32">
+                <div className="flex flex-row gap-8 items-center w-full h-fit">
+                    <div className="w-48">
                         <Avatar/>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <input
+                            type="file"
+                            className="file-input file-input-ghost w-full max-w-xs"
+                            accept="image/*"
+                            onChange={e => {
+                                uploadMedia(e.target.files[0]).then(r => {
+                                    if (r.status === "success") {
+                                        setUserData(data.username, {avatar: r.result.url}).then(result => {
+                                            console.log(result);
+                                            window.location.reload();
+                                        });
+                                    }
+                                });
+                            }}
+                        />
+                        <span className="text-white/50 mx-5">Must be JPG, PNG, JPEG and smaller 25MB</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="w-full h-fit bg-white/5 rounded-2xl">
+            <div className="p-10 flex flex-col gap-8">
+                <span className="font-extrabold text-2xl">Cover Picture</span>
+                <div className="flex flex-col gap-8 w-full h-fit">
+                    <div className="bg-black/50 flex items-center justify-center w-4/5 h-[400px] rounded-2xl">
+                        image
                     </div>
                     <div className="flex flex-col gap-2">
                         <input

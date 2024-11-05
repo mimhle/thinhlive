355f11c23ec684894039e3494285cdbc5d501103
NGUYỄN PHÚC THỊNH <119058000+hieu79115@users.noreply.github.com>
2024-11-05 16:46:08 +0700

    add loading image

--- src/app/(main)/setting/page.js
+++ src/app/(main)/setting/page.js
@@ -11,7 +11,8 @@ export default function Setting() {
     const [coverPhotoFile, setCoverPhotoFile] = useState(null);
     const [avatarPreview, setAvatarPreview] = useState(null);
     const [coverPhotoPreview, setCoverPhotoPreview] = useState(null);
-
+    const [isUpdatingAvatar, setIsUpdatingAvatar] = useState(false);
+    const [isUpdatingCoverPhoto, setIsUpdatingCoverPhoto] = useState(false);
 
     useEffect(() => {
         getUserData().then(r => {
@@ -22,100 +23,154 @@ export default function Setting() {
     }, []);
 
     useEffect(() => {
-        if(avatarFile){
+        if (avatarFile) {
             const previewUrl = URL.createObjectURL(avatarFile);
             setAvatarPreview(previewUrl);
             return () => URL.revokeObjectURL(previewUrl);
         }
-    }, [avatarFile])
+    }, [avatarFile]);
 
     useEffect(() => {
-        if(coverPhotoFile){
+        if (coverPhotoFile) {
             const previewUrl = URL.createObjectURL(coverPhotoFile);
             setCoverPhotoPreview(previewUrl);
             return () => URL.revokeObjectURL(previewUrl);
         }
-    }, [coverPhotoFile])
+    }, [coverPhotoFile]);
 
     if (!data) {
-        return <div
-            className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex justify-center p-20">
-        </div>
+        return (
+            <div
+                className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex justify-center p-20">
+            </div>
+        );
     }
 
     const avatarUpdate = () => {
         if (avatarFile) {
+            setIsUpdatingAvatar(true);
             uploadMedia(avatarFile).then(r => {
                 if (r.status === "success") {
                     setUserData(data.username, {avatar: r.result.url}).then(result => {
                         console.log(result);
-                        window.location.reload();
+                        setIsUpdatingAvatar(false);
                     });
+                } else {
+                    setIsUpdatingAvatar(false);
                 }
             });
         } else {
             alert("Please select a file for the profile picture.");
         }
-    }
+    };
 
     const coverPhotoUpdate = () => {
         if (coverPhotoFile) {
+            setIsUpdatingCoverPhoto(true);
             uploadMedia(coverPhotoFile).then(r => {
                 if (r.status === "success") {
                     setUserData(data.username, {coverphoto: r.result.url}).then(result => {
                         console.log(result);
-                        window.location.reload();
+                        setIsUpdatingCoverPhoto(false);
                     });
+                } else {
+                    setIsUpdatingCoverPhoto(false);
                 }
             });
         } else {
-            alert("Please select a file for the profile picture.");
+            alert("Please select a file for the cover photo.");
         }
-    }
+    };
 
-    return <div
-        className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex flex-col gap-4 py-20 px-40">
-        <div className="w-full h-fit bg-white/5 rounded-2xl">
-            <div className="p-10 flex flex-col gap-8">
-                <div className="flex justify-between">
-                    <span className="font-extrabold text-2xl">Profile Picture</span>
-                    <button className="btn btn-success" onClick={avatarUpdate}>Update profile Picture</button>
-                </div>
-                <div className="flex flex-row gap-8 items-center w-full h-fit">
-                    <div className="w-48">
-                        {avatarPreview ? <Avatar src={avatarPreview}/> : <Avatar/>}
+    return (
+        <div
+            className="h-screen w-screen bg-gradient-to-b from-neutral to-base-300 via-base-300 via-90% overflow-x-hidden flex flex-col gap-4 py-20 px-40">
+            <div className="w-full h-fit bg-white/5 rounded-2xl">
+                <div className="p-10 flex flex-col gap-8">
+                    <div className="flex justify-between items-center">
+                        <span className="font-extrabold text-2xl">Profile Picture</span>
+                        <button
+                            className="btn btn-outline btn-success"
+                            onClick={avatarUpdate}
+                            disabled={isUpdatingAvatar}
+                        >
+                            Update Profile Picture
+                        </button>
                     </div>
-                    <div className="flex flex-col gap-2">
-                        <input
-                            type="file"
-                            className="file-input file-input-ghost w-full max-w-xs"
-                            accept="image/*"
-                            onChange={e => setAvatarFile(e.target.files[0])}
-                        />
-                        <span className="text-white/50 mx-5">Must be JPG, PNG, JPEG and smaller 25MB</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="w-full h-fit bg-white/5 rounded-2xl">
-            <div className="p-10 flex flex-col gap-8">
-                <div className="flex justify-between">
-                    <span className="font-extrabold text-2xl">Cover Picture</span>
-                    <button className="btn btn-success" onClick={coverPhotoUpdate}>Update Cover Picture</button>
-                </div>
-                <div className="flex flex-col gap-8 w-full h-fit">
-                    {coverPhotoPreview ? <CoverPhoto src={coverPhotoPreview}/> : <CoverPhoto/> }
-                    <div className="flex flex-col gap-2">
-                        <input
-                            type="file"
-                            className="file-input file-input-ghost w-full max-w-xs"
-                            accept="image/*"
-                            onChange={e => {setCoverPhotoFile(e.target.files[0])}}
-                        />
-                        <span className="text-white/50 mx-5">Must be JPG, PNG, JPEG and smaller 25MB</span>
+                    <div className="flex flex-row gap-8 items-center w-full h-fit">
+                        <div className="w-48 relative">
+                            <Avatar src={avatarPreview || undefined}/>
+                            {isUpdatingAvatar && (
+                                <div
+                                    className="absolute inset-0 bg-black/50 flex items-center justify-center rounded-full">
+                                    <span className="loading loading-ring loading-lg"></span>
+                                </div>
+                            )}
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <input
+                                type="file"
+                                className="file-input file-input-ghost w-full max-w-xs"
+                                accept="image/*"
+                                onChange={e => setAvatarFile(e.target.files[0])}
+                            />
+                            <span className="text-white/50 mx-5">Must be JPG, PNG, JPEG and smaller 25MB</span>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className="w-full h-fit bg-white/5 rounded-2xl">
+                <div className="p-10 flex flex-col gap-8">
+                    <div className="flex justify-between items-center">
+                        <span className="font-extrabold text-2xl">Cover Picture</span>
+                        <button
+                            className="btn btn-outline btn-success"
+                            onClick={coverPhotoUpdate}
+                            disabled={isUpdatingCoverPhoto}
+                        >
+                            Update Cover Picture
+                        </button>
+                    </div>
+                    <div className="flex flex-col gap-8 w-full h-fit">
+                        <div className="relative">
+                            <CoverPhoto src={coverPhotoPreview || undefined}/>{
+                            isUpdatingCoverPhoto && (
+                                <div
+                                    className="absolute inset-0 bg-black/50 flex items-center justify-center rounded-2xl">
+                                    <span className="loading loading-ring loading-lg"></span>
+                                </div>
+                            )}
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <input
+                                type="file"
+                                className="file-input file-input-ghost w-full max-w-xs"
+                                accept="image/*"
+                                onChange={e => setCoverPhotoFile(e.target.files[0])}
+                            />
+                            <span className="text-white/50 mx-5">Must be JPG, PNG, JPEG and smaller 25MB</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full h-fit bg-white/5 rounded-2xl">
+                <div className="p-10 flex flex-col gap-8">
+                    <div className="flex justify-between items-center">
+                        <span className="font-extrabold text-2xl">Security</span>
+                        <button
+                            className="btn btn-outline btn-success"
+                            // onClick={coverPhotoUpdate}
+                            // disabled={isUpdatingCoverPhoto}
+                        >
+                            Update Password
+                        </button>
+                    </div>
+                    <div className="flex flex-col gap-8 w-full h-fit">
+
+                    </div>
+                </div>
+            </div>
+
         </div>
-    </div>
+    );
 }

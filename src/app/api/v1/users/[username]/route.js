50a7ea8df7a80f7c1cc806ba0305f6455adf6e7e
mimhle <40392166+mimhle@users.noreply.github.com>
2024-11-05 21:36:41 +0700

    Refactor

--- src/app/api/v1/users/[username]/route.js
+++ src/app/api/v1/users/[username]/route.js
@@ -1,7 +1,7 @@
 "use server";
 
 import { NextResponse } from "next/server";
-import { setUser } from "/lib/backend/auth";
+import { hashPassword, setUser, verifyUser } from "/lib/backend/auth";
 import { verifySession } from "/lib/backend/auth";
 
 export async function PUT(request) {
@@ -10,8 +10,18 @@ export async function PUT(request) {
     const body = await request.json();
     
     if (await verifySession(username, sessionId)) {
-        await setUser(username, body);
+        if (body.password && body["new_password"]) {
+            if (await verifyUser(username, body.password)) {
+                body.password = await hashPassword(body["new_password"]);
+            } else {
+                return NextResponse.json({ status: "failed", message: "Authentication failed" });
+            }
+        } else {
+            delete body.password;
+        }
+        delete body["new_password"];
 
+        await setUser(username, body);
         return NextResponse.json({ status: "success", result: body });
     }
     return NextResponse.json({ status: "failed", message: "Authentication failed" });

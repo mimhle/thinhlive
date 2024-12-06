392533079b118663b1cb2b90a32f0e48261a3e0c
mimhle <40392166+mimhle@users.noreply.github.com>
2024-12-06 13:59:49 +0700

    Refactor

--- src/app/api/v1/live/rooms/[room]/route.js
+++ src/app/api/v1/live/rooms/[room]/route.js
@@ -1,8 +1,7 @@
-import { RoomServiceClient } from 'livekit-server-sdk';
 import { NextResponse } from "next/server";
+import { roomService } from "/lib/backend/livekitRoomService";
 
 export async function GET(request) {
-    const roomService = new RoomServiceClient(process.env.NEXT_PUBLIC_LIVEKIT_SERVER_URL, process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_SECRET_KEY);
     const room = new URL(request.url).pathname.split('/').at(-1);
     const rooms = await roomService.listRooms();
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 import typography from "@tailwindcss/typography";

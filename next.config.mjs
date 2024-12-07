caf540a7d79c91af78426d1d5321f8cab32d00dc
mimhle <40392166+mimhle@users.noreply.github.com>
2024-12-07 18:46:00 +0700

    Disable cache

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store',
+                    },
+                ],
+            },
+        ]
+    },
 };
 
 export default nextConfig;
